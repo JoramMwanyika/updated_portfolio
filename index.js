49849753cd08188606dedd0a81bb7216a18cb0e4
Joram <joram.nyamawimwanyika@kibo.school>
2025-01-13 10:11:47 +0300

    improved device and browser compatibility

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function showNextProject() {
   project2.style.display = "block";
   window.scrollTo(0, project2.offsetTop);
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navMenu = document.querySelector(".nav__menu");
+
+  menuToggle.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+  });
+});
+
